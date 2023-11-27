ba746f0d43cc62156642133a905350fd8a78f664
joy-mohajon <mohajon33@gmail.com>
2023-11-27 23:46:32 +0600

    added loading spinner

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -6,14 +6,14 @@ const Spinner = () => {
     //  Spinner Start
     <div
       id="spinner"
-      class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center"
+      className="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center"
     >
       <div
-        class="spinner-border text-primary"
+        className="spinner-border text-primary"
         style="width: 3rem; height: 3rem"
         role="status"
       >
-        <span class="sr-only">Loading...</span>
+        <span className="sr-only">Loading...</span>
       </div>
     </div>
     //  Spinner End
