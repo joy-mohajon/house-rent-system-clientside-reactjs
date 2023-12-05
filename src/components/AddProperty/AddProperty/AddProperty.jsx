8008a88a32c6b4ec8db64fade79a401092817633
joy-mohajon <mohajon33@gmail.com>
2023-12-05 21:05:57 +0600

    updated resposive code of addproperty and progress bar

--- src/components/AddProperty/AddProperty/AddProperty.jsx
+++ src/components/AddProperty/AddProperty/AddProperty.jsx
@@ -8,7 +8,7 @@ const AddProperty = () => {
   const [currentStep, setCurrentStep] = useState(1);
 
   return (
-    <div className="page-wrapper p-t-100 p-b-100">
+    <div className="page-wrapper my-md-5 my-3 mx-3">
       <div className="wrapper wrapper--w680">
         <div className="card card-1">
           <div className="card-heading"></div>
